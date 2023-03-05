911aa17ca341f5e6bbebde74d14f0e234c8c8169
crazynavi <117359800+TrustedSeven@users.noreply.github.com>
2023-03-05 08:04:08 -0600

    routing, about working

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Navbar from "../components/navbar";
 
-import '../App.css'
+import "../App.css";
 
 export default function home() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          home
-        </p>
-      </header>
+      <div className="main-div">
+        <div className="navbar-top">
+          <Navbar />
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <button type="button" class="btn btn-primary">
+              Primary
+            </button>
+          </div>
+          <div className="col-md-6">
+            <Button>home</Button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import Button from "react-bootstrap/Button";
+import Navbar from "../components/navbar";
 
 import '../App.css'
 
 export default function projects() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          projects
-        </p>
-      </header>
+      <div className="main-div">
+        <div className="navbar-top">
+          <Navbar />
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <button type="button" class="btn btn-primary">
+              Projects
+            </button>
+          </div>
+          <div className="col-md-6">
+            <Button>projects</Button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
