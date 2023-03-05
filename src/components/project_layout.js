68d7dfbff92ab53f321c13d1110812fd81564b38
crazynavi <117359800+TrustedSeven@users.noreply.github.com>
2023-03-05 14:39:33 -0600

    initial complete

---
+++ src/components/project_layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function project_layout() {
+  return (
+    <div>
+        <p className="content-aboutme">
+              I believe that people should do things in which they are good at
+              or in which their heart lays. That's why I chose freelance web
+              development as my career because I believe I'm good at this and I
+              am always honest and I am never afraid of doing hard work. I
+              majored on Computer Science Engineering and I have taken many
+              lessons on modern web designing from famous institutions.I majored
+              on Computer Science Engineering and I have taken many lessons on
+              modern web designing from famous institutions.
+            </p>
+    </div>
+  )
+}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import Button from "react-bootstrap/Button";
+import React from "react";
 import Navbar from "../components/navbar";
 
-import '../App.css'
+import "../App.css";
+
+function Mailto({ email, subject, body, ...props }) {
+  return (
+    <a href={`mailto:${email}?subject=${subject || ""}&body=${body || ""}`}>
+      {props.children}
+    </a>
+  );
+}
 
 export default function contact() {
   return (
@@ -11,17 +18,29 @@ export default function contact() {
         <div className="navbar-top">
           <Navbar />
         </div>
-        <div className="row">
+        <div className="row margintop-std">
           <div className="col-md-6">
-            <button type="button" class="btn btn-primary">
-              Contact
-            </button>
+            <p className="name-sm-contact">Email</p>
           </div>
+          <div className="col-md-6 name-sm-contact">
+            <Mailto email="kleinbergbrian.dev@gmail.com" subject="Hello" body="Hello Brian!" obfuscate={true}>
+              kleinbergbrian.dev@gmail.com
+            </Mailto>
+          </div>
+        </div>
+        <div className="row margintop-std">
           <div className="col-md-6">
-            <Button>contact</Button>
+            <p className="name-sm-contact">Phone</p>
+            <p className="name-sm-contact">Linkedin</p>
+            <p className="name-sm-contact">Github</p>
+          </div>
+          <div className="col-md-6 name-sm-contact">
+            <p className="name-sm-contact">+18474566702</p>
+            <p className="name-sm-contact">https://linkedin.com/xxx</p>
+            <p className="name-sm-contact">https://github.com/xxx</p>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
