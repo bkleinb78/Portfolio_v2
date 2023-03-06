b31e21cb278c086d58566b47086ae6c10452596a
crazynavi <117359800+TrustedSeven@users.noreply.github.com>
2023-03-05 19:55:37 -0600

    navbar white, icon add, live demo add

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,10 +10,10 @@ function topNavbar() {
         <Navbar bg="dark" variant="dark">
           <Container>
             <Nav className="me-auto">
-              <Nav.Link href="/" className='nav-item'>HOME</Nav.Link>
-              <Nav.Link href="/about" className='nav-item'>ABOUT</Nav.Link>
-              <Nav.Link href="/contact" className='nav-item'>CONTACT</Nav.Link>
-              <Nav.Link href="/projects" className='nav-item'>PROJECTS</Nav.Link>
+              <Nav.Link href="/" className='nav-item'><a style={{color:"white"}}>HOME</a></Nav.Link>
+              <Nav.Link href="/about" className='nav-item'><a style={{color:"white"}}>ABOUT</a></Nav.Link>
+              <Nav.Link href="/projects" className='nav-item'><a style={{color:"white"}}>PROJECTS</a></Nav.Link>
+              <Nav.Link href="/contact" className='nav-item'><a style={{color:"white"}}>CONTACT</a></Nav.Link>
             </Nav>
           </Container>
         </Navbar>
