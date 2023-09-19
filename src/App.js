15a8474eb6105e68ee9881a8d17753b72d51f36d
cooldev900 <cooldev900@gmail.com>
2023-09-19 15:02:43 -0500

    fix: button position and text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import About from "./pages/about";
